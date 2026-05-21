fba24b15d434e9daaef42f96a3738bc84e1767dd
Jaber Molla <molla.jaber@gmail.com>
2026-05-22 02:25:44 +0600

    fixed desing issue on room details page

--- src/componenet/Room/RoomDetails.jsx
+++ src/componenet/Room/RoomDetails.jsx
@@ -14,7 +14,7 @@ const RoomDetails = async ({ session, room, roomOwner }) => {
   const { count } = await res.json();
 
   return (
-    <div className="container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-8 my-8">
+    <div className="container mx-auto grid grid-cols-1 lg:grid-cols-3 gap-8 py-8">
       {/* Left Content */}
       <div className="lg:col-span-2">
         {
